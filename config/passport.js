5c8aabe329dbba6ebbdd9914d6ede172fbb65ed5
Glitch (plant-kick) <none>
2018-06-05 18:25:45 +0000

    👸🌇 Checkpoint ./config/passport.js:1174538/736

--- config/passport.js
+++ config/passport.js
@@ -4,9 +4,39 @@ var Users=require('../models/users');
 module.exports=function(passport)
 {
 
-  passport.serializeUrl(user,done){
+  passport.serializeUrl(function(user,done){
   
-  }
+    done(null,user._id);
+  
+  })
+  
+    passport.deserializeUrl(function(id,done){
+  
+    Users.findById(id,function(err,user){
+    
+      done(null,user)
+      
+    })  
+  })
+  
+  
+  passport.use('login',new passportLocal(
+  
+  function(req,username,password,done)
+    {
+    Users.findOne({user:username},function(err,User)
+                  {
+    if(!User){
+    return done(null,false);
+    }
+      if(User.password!=passwor
+    
+    })
+    
+    
+    }
+  
+  ))
 
 
 }
